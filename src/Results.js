96c809cfd283a260a46164ef052a5be99274a687
ClareMorgan <claredotm@gmail.com>
2024-06-05 23:38:04 +0100

    Rendered phonetics for word searches

--- src/Results.js
+++ src/Results.js
@@ -1,4 +1,6 @@
 import React from "react";
+
+import Phonetics from "./Phonetics";
 import Meanings from "./Meanings";
 
 export default function Results(props) {
@@ -6,6 +8,13 @@ export default function Results(props) {
     return (
       <div className="Results">
         <h2>{props.results.word}</h2>
+        {props.results.phonetics.map(function (phonetics, index) {
+          return (
+            <div key={index}>
+              <Phonetics phonetics={phonetics} />
+            </div>
+          );
+        })}
         {props.results.meanings.map(function (meanings, index) {
           return (
             <div key={index}>
